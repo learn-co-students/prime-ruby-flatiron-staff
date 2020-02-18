e1a0005794f3d69d0674b19485a921e735bb7f00
Thu Do <thu.do@flatironschool.com>
2020-02-18 02:46:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  else
+    (2..(num-1)).each do |n|
+      return false if num % n == 0
+        n += 1
+      end
+      true
+  end
+end
