7ff4e109b8bcfea288a52c6999324a2693ed352d
Anna Lux <anna.watt@flatironschool.com>
2020-04-22 12:13:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+require 'pry'
+
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number == 2
+    return true
+  elsif
+    factors = (2...number).to_a
+    factors.each { | factor |
+      if number % factor == 0
+        return false
+      end
+      }
+    return true
+  end
+end
